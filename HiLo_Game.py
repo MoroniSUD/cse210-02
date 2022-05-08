ea53210ef7782cc30ead88420241d04c0292bc8d
Dario <58082692+MoroniSUD@users.noreply.github.com>
2022-05-07 23:30:12 -0300

    The code is complete with one class and two methods.

--- HiLo_Game.py
+++ HiLo_Game.py
@@ -1,35 +1,53 @@
 import  random
 
-name = input("Por favor ingrese su nombre: ")
+class Card:
+        
+    def __init__(self, name):
+        self.score = 300
+        self.first_card = 0
+        self.second_card = 0
+        self.name = name
 
-puntos = 300
 
-primera_carta = 0
-segunda_carta = 0
+    def Play(self):
+        choose = 'y'
+        
+        while choose.lower() == 'y':
 
-play = 'y'
+            first_card = random.randint(1,13)
+            second_card = random.randint(1,13)
+            
+            print(f'\nThe first card is: {first_card}')
+            option = input(f'{self.name}, la proxima carta es mas alta o baja (h o l): ')
+            print(f'The next card is: {second_card}')
+            if first_card < second_card and option.lower() == 'h':
+                self.score += 100
+                print(f'Won. Your score {self.name} is {self.score}')
+            elif first_card > second_card and option.lower() == 'l':
+                self.score += 100
+                print(f'Won. Your score is {self.score}')
+            elif first_card == second_card:
+                print(f'Cards are the same. Your score is {self.score}')
+            else:
+                self.score -= 75 
+                print(f'Lost. Your score is {self.score}')   
+            if self.score <= 0:
+                print(f'You lost. The end. Your score is {self.score}')
+                choose = 'n'
+            else:    
+                choose = input(f'{self.name} want play again (y/n): ')
+                if choose.lower() != 'y':
+                    print(f'Thank you for playing. The final score is {self.score}\n.')
 
-while play.lower() == 'y':
-    primera_carta = random.randint(1,13)
-    segunda_carta = random.randint(1,13)
-    print(f'\nThe first card is: {primera_carta}')
-    opcion = input(f'{name}, la proxima carta es mas alta o baja (h o l): ')
-    print(f'The next card is: {segunda_carta}')
-    if primera_carta < segunda_carta and opcion.lower() == 'h':
-        puntos += 100
-        print(f'Won. Your score {name} is {puntos}')
-    elif primera_carta > segunda_carta and opcion.lower() == 'l':
-        puntos += 100
-        print(f'Won. Your score is {puntos}')
-    elif primera_carta == segunda_carta:
-        print(f'Cards are the same. Your score is {puntos}')
-    else:
-        puntos -= 75 
-        print(f'Lost. Your score is {puntos}')   
-    if puntos <= 0:
-        print(f'You lost. The end. Your score is {puntos}')
-        play = 'n'
-    else:    
-        play = input(f'{name} want play again (y/n): ')
-        if play.lower() != 'y':
-           print(f'Thank you for playing. The final score is {puntos}\n.') 
+
+
+def main():
+
+    name = input("Please enter your name: ")
+    tarjeta = Card(name)
+    tarjeta.Play()
+
+# Call main to start this program.
+if __name__ == "__main__":
+    main()
+
