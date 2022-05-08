7e27773ca6b4a01145257a7d35201e1f1281362e
Dario <58082692+MoroniSUD@users.noreply.github.com>
2022-05-08 00:26:51 -0300

    I end the program with the comments.

--- HiLo_Game.py
+++ HiLo_Game.py
@@ -1,8 +1,22 @@
+#I call the random library for the random values of the cards.
 import  random
 
+
 class Card:
-        
+    """Card class to play
+    Card's responsibility is to develop the game in all its stages.
+    Attributes:
+        score: the points with which the player starts and achieves.
+        first_card: the first random value of the game.
+        second_card: the first random value of the game.
+        name: name of the person playing the game.
+    """        
     def __init__(self, name):
+        """Constructs a new Card.
+        
+        Args:
+            self (Card): an instance of Card.
+        """
         self.score = 300
         self.first_card = 0
         self.second_card = 0
@@ -10,16 +24,26 @@ class Card:
 
 
     def Play(self):
+        """Starts the game by running the main game.
+        
+        Args:
+            self (Card): an instance of Card.
+
+        """
         choose = 'y'
         
+        #evaluate the option to continue playing
         while choose.lower() == 'y':
 
+            #Randomly assigning values to the cards
             first_card = random.randint(1,13)
             second_card = random.randint(1,13)
             
             print(f'\nThe first card is: {first_card}')
             option = input(f'{self.name}, la proxima carta es mas alta o baja (h o l): ')
             print(f'The next card is: {second_card}')
+
+            # Depending on the option entered by the user, the comparison of the two charts is evaluated as high or low.
             if first_card < second_card and option.lower() == 'h':
                 self.score += 100
                 print(f'Won. Your score {self.name} is {self.score}')
@@ -39,10 +63,8 @@ class Card:
                 if choose.lower() != 'y':
                     print(f'Thank you for playing. The final score is {self.score}\n.')
 
-
-
 def main():
-
+    #I call the classes and their methods. I request the player's name
     name = input("Please enter your name: ")
     tarjeta = Card(name)
     tarjeta.Play()
